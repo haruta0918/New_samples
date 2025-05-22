0d71cedaf8a289e7c80af1297230e0b8f5199c78
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-05-22 20:37:38 +0900

    New

--- hello.py
+++ hello.py
@@ -2,16 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df1 = pd.DataFrame(
-    np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
 )
-
-my_table = st.table(df1)
-
-df2 = pd.DataFrame(
-    np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
-)
-
-my_table.add_rows(df2)
-# Now the table shown in the Streamlit app contains the data for
-# df1 followed by the data for df2.
+st.map(df)
