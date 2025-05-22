6087dc96ddfa439d37062db2ea5eaa6d2593ad1e
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-05-23 06:43:42 +0900

    aaa

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-    (200, 2) / [350, 350] + [34.712531, 137.6887006],
+    np.random.randn(200, 2) / [350, 350] + [34.712531, 137.6887006],
     columns=["lat", "lon"],
 )
 st.map(df, size=5, color="#0044ff")
