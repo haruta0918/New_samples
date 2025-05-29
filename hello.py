4494a65396503984bd29587151444f61b2093879
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-05-29 20:16:14 +0900

    wish Last

--- hello.py
+++ hello.py
@@ -6,10 +6,8 @@ df = pd.DataFrame(
     {
         "col1":   200 + 34.715265,
         "col2":  200 + 137.6885,
-        "col3": 11,
+        "col3": 11
     }
 )
 
 st.map(df, latitude="col1", longitude="col2", size="col3", color="#0044ff")
-st.map(df, size=10, color="#0044ff")
-st.map(df, size=10, color="#0044ff")
