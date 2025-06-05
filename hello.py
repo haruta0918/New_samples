6cfba5657a3e2630780d95decc85656eeae23053
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-05 21:01:02 +0900

    New

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 
 d = [
-    {"LAT": 34.71946333, "LON": 137.69348144, "SIZE": 1, "COLOR": [0, 0, 255, 100]},
-    {"LAT": 34.71929167, "LON": 137.69297789, "SIZE": 1, "COLOR": [0, 255, 0, 100]},
-    {"LAT": 34.71929167, "LON": 137.69297789, "SIZE": 1, "COLOR": [255, 0, 0, 100]},
+    {"LAT": 34.71946333, "LON": 137.69348144, "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+    {"LAT": 34.71929167, "LON": 137.69297789, "SIZE": 0.5, "COLOR": [0, 255, 0, 100]},
+    {"LAT": 34.71917342, "LON": 137.69277953, "SIZE": 0.5, "COLOR": [255, 0, 0, 100]},
 ]
 
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
