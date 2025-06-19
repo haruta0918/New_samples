a6b596b776cd9c3b7b28bf10b6eba1ab67336167
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-19 20:44:58 +0900

    aaaa

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ d = [
     ]
 json_open = open('samples/20250427_152001.jpg.supplemental-metadata.json', 'r')
 json_load = json.load(json_open)
-d.extend(
+d(
      {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
 )
 
