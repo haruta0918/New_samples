0f7237d6db63f12a8754c86f620ce17d75c300de
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-20 06:43:38 +0900

    Wish

--- hello.py
+++ hello.py
@@ -6,14 +6,15 @@ import json
 json_open = open('samples/20250415_165815.jpg.supplemental-metadata.json', 'r')
 json_load = json.load(json_open)
 
-d = [
+d = {
+    "points": [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
+}
 
-
-d.extend = ([
-    {"LAT": 34.704852848835955, "LON": 137.68991679239386, "SIZE": 0.6, "COLOR": [0, 255, 0, 100]},
-    {"LAT": 34.70681773800157, "LON": 137.6818660117991, "SIZE": 0.6, "COLOR": [0, 255, 0, 100]},
+new_point = ([
+    {"LAT": 34.704852848835955, "LON": 137.68991679239386, "SIZE": 0.5, "COLOR": [0, 255, 0, 100]},
+    {"LAT": 34.70681773800157, "LON": 137.6818660117991, "SIZE": 0.5 , "COLOR": [0, 255, 0, 100]},
 ])
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
 print(d)
--- new.py
+++ new.py
@@ -6,10 +6,7 @@ import json
 json_open = open('samples/20250415_165815.jpg.supplemental-metadata.json', 'r')
 json_load = json.load(json_open)
 
-d = [
-        {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
-    ]
-d.append = [
-        {"LAT": 34.0522, "LON": -118.2437, "SIZE": 0.6, "COLOR": [0, 255, 0, 100]},
-        {"LAT": 34.0522, "LON": -118.2437, "SIZE": 0.6, "COLOR": [0, 255, 0, 100]},
-    ]
+
+new_point = {"LAT": 34.9, "LON": 137.8, "SIZE": 0.5, "COLOR": [255, 0, 0, 100]}
+d["points"].append(new_point)
+print(d["points"])  # すべてのポイントリストを表示
