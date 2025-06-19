5fe5b42dea8a311571b4a57092a6cbfbd312a9fc
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-19 20:45:55 +0900

    aaaa# Please enter the commit message for your changes. Lines starting

--- hello.py
+++ hello.py
@@ -11,8 +11,8 @@ d = [
     ]
 json_open = open('samples/20250427_152001.jpg.supplemental-metadata.json', 'r')
 json_load = json.load(json_open)
-d(
-     {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+d.extend(
+     {"LAT":json_load[2]['geoData']['latitude'], "LON":json_load[2]['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
 )
 
 st.map(d[2], latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
