09533aba36610ed4a707d94992d66bd16145cac7
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-19 19:37:26 +0900

    New

--- hello.py
+++ hello.py
@@ -2,17 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
-json_open = open('samples/20250415_165815.jpg.supplemental-metadata.json', 'r')
+json_open = open('samples/20250415_165815.jpg.supplemental-metadata', 'r')
 json_load = json.load(json_open)
 
 d = [
     {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
 ]
-st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
-json_open = open('samples/20250415_170934.jpg.supplemental-metadata.json', 'r')
+json_open = open('20250426_134824.jpg.supplemental-metadata', 'r')
 json_load = json.load(json_open)
 
-d = [
-    {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
-]
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
+print(d)
