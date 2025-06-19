89838e2ace5c412b22c73ec6c24b25d28126f9cf
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-20 07:26:57 +0900

    angry

--- hello.py
+++ hello.py
@@ -8,4 +8,13 @@ json_load = json.load(json_open)
 d = [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
+# 1つの辞書として追加
+lat = json_load['geoData']['latitude']
+lon = json_load['geoData']['longitude']
+new_point = {
+    "LAT": lat,
+    "LON": lon,
+    "SIZE": 0.5,
+    "COLOR": [0, 255, 0, 100]
+}
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
