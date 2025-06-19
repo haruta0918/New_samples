ad190d8199a8ffd5fbeb571fe696f91dcf7df2a4
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-19 19:42:46 +0900

    Wish

--- hello.py
+++ hello.py
@@ -8,8 +8,6 @@ json_load = json.load(json_open)
 d = [
     {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
 ]
-json_open = open('20250426_134824.jpg.supplemental-metadata.json', 'r')
-json_load = json.load(json_open)
 
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
 print(d)
