48d8f1faa8791f662d3c73e4e9f39237301f41bf
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-21 08:05:08 +0900

    Now

--- hello.py
+++ hello.py
@@ -3,23 +3,12 @@ import pandas as pd
 import numpy as np
 import json
 
-with open('samples/20250427_152756.jpg.supplemental-metadata.json', 'r', encoding='utf-8') as f1:
-    data1 = json.load(f1)
-
+json_open=open('samples/20250427_152756.jpg.supplemental-metadata.json', 'r') 
+json_load = json.load(json_open)
 
     
 d = [
-        {"LAT":f1['geoData']['latitude'], "LON":f1['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+        {"LAT":json_load['geoData']['latitude'], "LON":f1['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
 # 1つの辞書として追加
-with open('samples/20250427_150440.jpg.supplemental-metadata.json', 'r', encoding='utf-8') as f2:
-    data2 = json.load(f2)
-lat = f2['geoData']['latitude']
-lon = f2['geoData']['longitude']
-new_point = {
-    "LAT": lat,
-    "LON": lon,
-    "SIZE": 0.5,
-    "COLOR": [0, 255, 0, 100]
-}
-st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)(new_point,latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15),
+st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
--- new.py
+++ new.py
@@ -2,19 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
+
+with open('samples/20250427_152756.jpg.supplemental-metadata.json', 'r', encoding='utf-8') as f1:
+    data1 = json.load(f1)
+
     
-json_open = open('samples/20250415_165815.jpg.supplemental-metadata.json', 'r')
-json_load = json.load(json_open)
-
-d = {
-    "points": [
-        {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+d = [
+        {"LAT":f1['geoData']['latitude'], "LON":f1['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
+# 1つの辞書として追加
+with open('samples/20250427_150440.jpg.supplemental-metadata.json', 'r', encoding='utf-8') as f2:
+    data2 = json.load(f2)
+lat = f2['geoData']['latitude']
+lon = f2['geoData']['longitude']
+new_point = {
+    "LAT": lat,
+    "LON": lon,
+    "SIZE": 0.5,
+    "COLOR": [0, 255, 0, 100]
 }
-
-new_point = [
-    {"LAT": 34.704852848835955, "LON": 137.68991679239386, "SIZE": 0.5, "COLOR": [0, 255, 0, 100]},
-]
-d["points"].append(new_point)
-st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
-print(d)
+st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)(new_point,latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15),
