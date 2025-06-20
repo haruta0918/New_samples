8178bc134c74538d5cd21846efc04438f0414af1
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-20 22:10:43 +0900

    Now

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ d = [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
 # 1つの辞書として追加
-json_open1 = open('sample/20250427_152127.jpg.supplemental-metadata.json', 'r')
+json_open1 = open('sample/20250415_170700.jpg.supplemental-metadata.json', 'r')
 json_load1 = json.load(json_open1)
 lat = json_load1['geoData']['latitude']
 lon = json_load1['geoData']['longitude']
