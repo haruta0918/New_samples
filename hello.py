a32aae5fe3aa7ae9aaa0d2988939962cf073a832
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-20 16:30:07 +0900

    New

--- hello.py
+++ hello.py
@@ -9,10 +9,10 @@ d = [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
 # 1つの辞書として追加
-json_open = open('sample/20250427_152127.jpg.supplemental-metadata.json', 'r')
-json_load = json.load(json_open)
-lat = json_load['geoData']['latitude']
-lon = json_load['geoData']['longitude']
+json_open1 = open('sample/20250427_152127.jpg.supplemental-metadata.json', 'r')
+json_load1 = json.load(json_open1)
+lat = json_load1['geoData']['latitude']
+lon = json_load1['geoData']['longitude']
 new_point = {
     "LAT": lat,
     "LON": lon,
