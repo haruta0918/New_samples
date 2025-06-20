847f30ed271d9514edeb92f69c48af493ffb01ec
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-21 08:28:43 +0900

    Wish

--- hello.py
+++ hello.py
@@ -5,16 +5,16 @@ import json
 
 json_open=open('samples/20250427_152756.jpg.supplemental-metadata.json', 'r') 
 json_load = json.load(json_open)
+json_open1=open('samples/20250427_152001.jpg.supplemental-metadata.json', 'r') 
+json_load1 = json.load(json_open)
 
 d = [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+        {"LAT":json_load1['geoData']['latitude'], "LON":json_load1['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
 
 json_open=open('samples/20250427_152001.jpg.supplemental-metadata.json', 'r') 
 json_load = json.load(json_open)
 
-d = [
-        {"LAT1":json_load['geoData']['latitude'], "LON1":json_load['geoData']['longitude'], "SIZE1": 0.5, "COLOR1": [0, 0, 255, 100]},
-    ]
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)(d, latitude="LAT1", longitude="LON1", size="SIZE1", color="COLOR1", zoom=15)
