1bec76ef529964744d9bdbfac47c6b59513bc190
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-06-21 08:20:01 +0900

    New

--- hello.py
+++ hello.py
@@ -10,3 +10,11 @@ d = [
         {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
     ]
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
+
+json_open=open('samples/20250427_152001.jpg.supplemental-metadata.json', 'r') 
+json_load = json.load(json_open)
+
+d = [
+        {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+    ]
+st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
--- new.py
+++ new.py
@@ -22,3 +22,17 @@ new_point = {
     "COLOR": [0, 255, 0, 100]
 }
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)(new_point,latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15),
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+
+json_open=open('samples/20250427_152756.jpg.supplemental-metadata.json', 'r') 
+json_load = json.load(json_open)
+
+d = [
+        {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]},
+    ]
+st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
