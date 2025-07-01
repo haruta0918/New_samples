d9cc157d7731bf91c6d28c3a9c7bbb169577af15
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-07-01 21:55:49 +0900

    Wish

--- hello.py
+++ hello.py
@@ -8,34 +8,37 @@ import os
 
 folder_path = 'samples'
 
-# .jsonファイルを取得して並び替え（オプション）
-json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
-json_files.sort()  # ソートすると見やすい順に並ぶ
-
-folder_path2=folder_path+'/'
-a2=0
-a=json_files[a2]
-print(a)
-stra=str(a)
-c=folder_path2+stra
-json_open=open(c, 'r',encoding='utf-8') 
-json_load = json.load(json_open)
-d = [
-        {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]}, 
-    ]
-folder_path = 'samples'
+t=0
 # フォルダ内の .json ファイルを探してループ
 for filename in os.listdir(folder_path):
     if filename.endswith('.json'):
-        a2+=1
-        a=json_files[a2]
-        stra=str(a)
-        c=folder_path2+stra
-        file_path = os.path.join(folder_path, filename)
-        json_open=open(c, 'r',encoding='utf-8') 
-        json_load = json.load(json_open)
-        print(d)
-        d.append({"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]})       
+        if t==0:
+            # .jsonファイルを取得して並び替え（オプション）
+            json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+            json_files.sort()  # ソートすると見やすい順に並ぶ
+
+            folder_path2=folder_path+'/'    
+            a2=0
+            a=json_files[a2]
+            print(a)
+            stra=str(a)
+            c=folder_path2+stra
+            json_open=open(c, 'r',encoding='utf-8') 
+            json_load = json.load(json_open)
+            d = [
+                    {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]}, 
+                ]
+        else:
+            a2+=1
+            a=json_files[a2]
+            stra=str(a)
+            c=folder_path2+stra
+            file_path = os.path.join(folder_path, filename)
+            json_open=open(c, 'r',encoding='utf-8') 
+            json_load = json.load(json_open)
+            print(json_load['title'])
+            d.append({"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]})
+            t+=1       
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
 print(d)
 
