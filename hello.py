749ddc701fa71243c03ace0c2eb688def1fa0162
haruta0918 <176218758+haruta0918@users.noreply.github.com.>
2025-07-02 05:50:25 +0900

    New

--- hello.py
+++ hello.py
@@ -7,12 +7,16 @@ import os
 import os
 
 folder_path = 'samples'
-
+o=0
 t=0
+
 # フォルダ内の .json ファイルを探してループ
 for filename in os.listdir(folder_path):
     if filename.endswith('.json'):
+        o+=1
+        print(o)
         if t==0:
+            print("1回目")
             # .jsonファイルを取得して並び替え（オプション）
             json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
             json_files.sort()  # ソートすると見やすい順に並ぶ
@@ -29,6 +33,7 @@ for filename in os.listdir(folder_path):
                     {"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]}, 
                 ]
         else:
+            print("2回目以降")
             a2+=1
             a=json_files[a2]
             stra=str(a)
@@ -36,9 +41,9 @@ for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
             json_open=open(c, 'r',encoding='utf-8') 
             json_load = json.load(json_open)
-            print(json_load['title'])
             d.append({"LAT":json_load['geoData']['latitude'], "LON":json_load['geoData']['longitude'], "SIZE": 0.5, "COLOR": [0, 0, 255, 100]})
-            t+=1       
+        t+=1
+
 st.map(d, latitude="LAT", longitude="LON", size="SIZE", color="COLOR", zoom=15)
 print(d)
 
